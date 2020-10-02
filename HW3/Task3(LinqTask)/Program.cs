45a47fd3551e41c5cfc9b33ebfd8cb104ec90eab
Diana-Govorova <diana.govorova2001@gmail.com>
2020-10-03 02:51:17 +0300

    Add task 3 and tests for task 3.

---
+++ HW3/Task3(LinqTask)/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task3_LinqTask_
+{
+    class Program
+    {
+        static void Main()
+        {
+            BusinessLogic businessLogic = new BusinessLogic();
+        }
+    }
+}
---
+++ HW3/Task3(LinqTask)/Record.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task3_LinqTask_
+{
+    public class Record
+    {
+        public User Author { get; set; }
+
+        public String Message { get; set; }
+
+        public Record(User author, String message)
+        {
+            this.Author = author;
+            this.Message = message;
+        }
+    }
+}
